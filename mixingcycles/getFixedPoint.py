56907f6f46107b488732bc6d3556434232d8e23c
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-05-04 11:47:46 +0300

    working version with NR iteration

--- mixingcycles/getFixedPoint.py
+++ mixingcycles/getFixedPoint.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import numpy as np
 import argparse
 import sys,math
@@ -28,13 +27,14 @@ def getGrowthMatrix(parameters,size = 300):
         else:
             return 0
         
-    mn = np.zeros((size,size,2))
+    mn1 = np.zeros((size,size))
+    mn2 = np.zeros((size,size))
     for i in range(size):
         for j in range(size):
             t = get_time_to_substrate_depletion(i,j)
-            mn[i,j] = np.array([i,j]) * np.exp(p['growth']*t) * p['dilution']
+            mn1[i,j],mn2[i,j] = np.array([i,j]) * np.exp(p['growth']*t) * p['dilution']
 
-    return mn
+    return mn1,mn2
 
 def prob(m,n):
     if n[0] > 0:
@@ -61,7 +61,9 @@ parser.add_argument("-d","--dilutionfactor",type=float,default=2e-4)
 parser.add_argument("-T","--mixingtime",type=float,default=12.)
 parser.add_argument("-M","--maxiterations",type=int,default=1000)
 parser.add_argument("-A","--alpha",type=float,default=1.)
-parser.add_argument("-p","--precision",type=float,default=1e-20)
+parser.add_argument("-p","--precision",type=float,default=1e-14)
+parser.add_argument("-v","--verbose",action="store_true",default=False)
+parser.add_argument("-N","--newtonraphson",action="store_true",default=False)
 args = parser.parse_args()
 
 
@@ -74,18 +76,53 @@ p['substrate']  = args.substrateconcentration
 p['mixingtime'] = args.mixingtime
 p['dilution']   = args.dilutionfactor
 
-growthm = getGrowthMatrix(p,size = args.maxM)
-
+# initialize necessary variables
+growth1,growth2 = getGrowthMatrix(p,size = args.maxM)
 m = np.arange(args.maxM)
+j = np.zeros((2,2))
+
+# initial condition are the respective fixed points on the axis
 n = p['dilution']/(1-p['dilution'])*p['substrate']*p['yield']
 
 for i in range(args.maxiterations):
-    print "{:4d} {:12.8f} {:12.8f}".format(i,n[0],n[1])
-    px,py = prob(m,n)
-    # construct iteration function
-    fn = np.dot(px,np.dot(py,growthm))
-    if np.sum((n-fn)**2) < args.precision:
-        break
-    n = fn
+    if args.verbose:
+        print "{:4d} {:12.8f} {:12.8f}".format(i,n[0],n[1])
     
+    # poisson probabilities for seeding new droplets
+    px,py = prob(m,n)
+    
+    # construct iteration function for growth and dilution by summing over all possibilities of how droplets are seeded
+    fn = np.array([np.dot(py,np.dot(px,growth1)),np.dot(py,np.dot(px,growth2))])
+    
+    if args.newtonraphson:
+        # NR iterations 
+        # generate derivatives with respect to variables for NR, dP[m|n]/dn
+        if n[0] > 0:    dpx = (m/n[0] - 1.)*px
+        else:           dpx = -px
+        if n[1] > 0:    dpy = (m/n[1] - 1.)*py
+        else:           dpy = -py
+
+        # get jacobian of dynamics
+        j[0,0] = np.dot(dpy,np.dot(px,growth1))-1.
+        j[0,1] = np.dot(py,np.dot(dpx,growth1))
+        j[1,0] = np.dot(dpy,np.dot(px,growth2))
+        j[1,1] = np.dot(py,np.dot(dpx,growth2))-1.
+        
+        # calculate step in NR iteration
+        dn = -args.alpha * np.dot(np.linalg.inv(j),fn-n)
+    
+    else:
+        # simple iterations of the function, hoping it converges at some point
+        dn = fn-n
+    
+    n += dn
+    n[n<0] = 0
+    
+    # stop condition met? is the change small enough?
+    if np.sum((dn[n>0]/n[n>0])**2) < args.precision:
+        break
+
+# final output
+print p['growth'][0]/p['growth'][1],p['yield'][0]/p['yield'][1],n[0],n[1]
+
 
