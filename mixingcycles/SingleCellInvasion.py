d634697049759470aa5550cfc21edb3d0ded922b
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-07-28 17:38:39 +0300

    small changes

--- mixingcycles/SingleCellInvasion.py
+++ mixingcycles/SingleCellInvasion.py
@@ -15,12 +15,13 @@ args = parser.parse_args()
 
 g     = growthdynamics(growthrates = np.array(args.growthrates), yieldrates = np.array(args.yieldrates), mixingtime = args.mixingtime, dilution = args.dilutionfactor, substrate = args.substrateconcentration)
 
-m = np.arange(1000)
+m = np.arange(100)
 step = 1
-last=1000
+last=100
+
 while last <= args.maxM:
-    n0,n1 = g.getGrowthMatrix(size = (m,np.array([1.,0.])))
-    for data in zip(m,n0,n1):
+    n1,n2 = g.getGrowthMatrix(size = (m,np.array([1.,0.])))
+    for data in zip(m,n1,n2):
         print "{:6d} {:15.10f} {:15.10f} {:15.10f}".format(data[0],data[1][0],data[2][0],data[1][1])
     step *= 10
     first = last
