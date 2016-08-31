ac6d5258afe15e41c24de6b691533797538ec944
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-08-31 18:15:06 +0300

    program to estimate mixing parameters

---
+++ mixingcycles/getGamma.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys,math
+import numpy as np
+import growthclasses as gc
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-D","--delta",type=float,default=.2)
+args = parser.parse_args()
+
+
+g = gc.GrowthDynamics(dilution = 1.)
+
+for yexp in np.arange(-1,1+args.delta,args.delta):
+    y = np.array([10**yexp,1.])
+    g.setYieldRates(y)
+    for aexp in np.arange(-1,1+args.delta,args.delta):
+        a = np.array([10**aexp,1.])
+        g.setGrowthRates(a)
+        
+        n11 = g.getGrowth(np.array([2,0]))
+        n22 = g.getGrowth(np.array([0,2]))
+        n12 = g.getGrowth(np.array([1,1]))
+        
+        print("{:10.6f} {:10.6f} {:.12f} {:.12f}".format(y[0],a[0],n12[0]/n11[0],n12[1]/n22[1]))
+        
+    print()
+
+
+
