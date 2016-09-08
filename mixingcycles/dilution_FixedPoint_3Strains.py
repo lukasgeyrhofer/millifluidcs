bca950b8c00c8032f7fb1eb81af0635834ac46b7
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-09-08 15:54:16 +0300

    slight changes to get production version

--- mixingcycles/dilution_FixedPoint_3Strains.py
+++ mixingcycles/dilution_FixedPoint_3Strains.py
@@ -6,6 +6,13 @@ import sys,math
 
 import growthclasses as gc
 
+
+def re(x):
+    return float(np.real(x))
+def im(x):
+    return float(np.imag(x))
+
+
 parser = argparse.ArgumentParser()
 parser = gc.AddGrowthParameters(parser,defaultgrowthrates = [1.,1.,1.],defaultyieldfactors = [1.,1.,1.],defaultdeathrates = [0.,0.,0.],dilution = True)
 parser.add_argument("-m","--maxseeding",type=int,default=200)
@@ -13,16 +20,13 @@ parser.add_argument("-p","--precision",type=float,default=1e-10)
 parser.add_argument("-A","--alpha",type=float,default=1)
 parser.add_argument("-M","--maxiterations",type=int,default=10000)
 parser.add_argument("-N","--newtonraphson",action="store_true",default=False)
-parser.add_argument("-v","--verbose",action="store_true",default=False)
 args = parser.parse_args()
 
 g = gc.GrowthDynamics(**vars(args))
 
 assert g.numstrains == 3
 
-print("# creating growth matrices ...")
 g1,g2,g3 = g.getGrowthMultipleStrains(size = args.maxseeding,nstrains = g.numstrains)
-print("# growth matrices done ...")
 
 m = np.arange(args.maxseeding)
 J = np.zeros((3,3))
@@ -68,4 +72,20 @@ while np.sum((dn[n>0]/n[n>0])**2) > args.precision:
     i += 1
 
 
-    print("{:5d} {:10.6f} {:10.6f} {:10.6f}".format(i,n[0],n[1],n[2]))
+J[0,0] = np.dot( px[2], np.dot( px[1], np.dot(dpx[0],g1)))
+J[0,1] = np.dot( px[2], np.dot(dpx[1], np.dot( px[0],g1)))
+J[0,2] = np.dot(dpx[2], np.dot( px[1], np.dot( px[0],g1)))
+                                                
+J[1,0] = np.dot( px[2], np.dot( px[1], np.dot(dpx[0],g2)))
+J[1,1] = np.dot( px[2], np.dot(dpx[1], np.dot( px[0],g2)))
+J[1,2] = np.dot(dpx[2], np.dot( px[1], np.dot( px[0],g2)))
+                                                
+J[2,0] = np.dot( px[2], np.dot( px[1], np.dot(dpx[0],g3)))
+J[2,1] = np.dot( px[2], np.dot(dpx[1], np.dot( px[0],g3)))
+J[2,2] = np.dot(dpx[2], np.dot( px[1], np.dot( px[0],g3)))
+
+
+w,v = np.linalg.eig(J)
+
+print("{growthrate1:5.3f} {yieldfactor1:5.3f} {growthrate2:5.3f} {yieldfactor2:5.3f} {fixedpoint0:10.6f} {fixedpoint1:10.6f} {fixedpoint2:10.6f} {eigenvalue0:10.6f} {eigenvalue1:10.6f} {eigenvalue2:10.6f} {iterationtime:5d}".format(g.growthrates[1],g.yieldfactors[1],g.growthrates[2],g.yieldfactors[2],n[0],n[1],n[2],re(w[0]),re(w[1]),re(w[2]),i)
+
