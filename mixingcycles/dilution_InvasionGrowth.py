3ad2595e01c9a4a1d19f7ed77d832845db94ca56
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-09-14 11:35:41 +0300

    program to estimate effects of single cell invasions in dilution seeding

---
+++ mixingcycles/dilution_InvasionGrowth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import argparse
+import numpy as np
+import sys,math
+
+import growthclasses as gc
+
+parser = argparse.ArgumentParser()
+parser = gc.AddGrowthParameters(parser,dilution=True)
+parser.add_argument("-m","--maxN",type=int,default=200)
+args = parser.parse_args()
+
+
+g = gc.GrowthDynamics(**vars(args))
+
+m = np.arange(args.maxN)
+
+growth1,growth2 = g.getGrowthMatrix(size=(m,np.array([0,1])))
+
+
+for i in range(args.maxN):
+    px = gc.PoissonSeedingVectors(m,np.array([i]))[0]
+    print("{:5d} {:.10f} {:.10f}".format(i,growth1[i,1],np.dot(px,growth1[:,1])))
+
+
