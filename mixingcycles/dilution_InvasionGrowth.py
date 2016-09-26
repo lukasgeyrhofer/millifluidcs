d5a07a7b8b0bf97ed11a866707e6c2ce65873e75
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-09-26 17:19:44 +0300

    simplified program

--- mixingcycles/dilution_InvasionGrowth.py
+++ mixingcycles/dilution_InvasionGrowth.py
@@ -22,8 +22,8 @@ px,dpx = gc.PoissonSeedingVectors(m,fp,diff=True)
 Egm1 = np.dot(g1m1[:,1],px[0])
 Eg1m = np.dot(g2m2[1,:],px[1])
 
-gamma1 = g.Growth([fp[0],1])[0]/g.Growth([fp[0],0])[0]
-gamma2 = g.Growth([1,fp[1]])[1]/g.Growth([0,fp[1]])[1]
+gamma1 = g.Growth([fp[0],1])[0]/fp[0]
+gamma2 = g.Growth([1,fp[1]])[1]/fp[1]
 
 print("{:f} {:f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}".format(g.growthrates[1]/g.growthrates[0],g.yieldfactors[1]/g.yieldfactors[0],Egm1,gamma1*fp[0],gamma1,fp[0],Eg1m,gamma2*fp[1],gamma2,fp[1]))
 
