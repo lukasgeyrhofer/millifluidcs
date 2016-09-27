f551d97389b61039ef5437e74eec0ff8e6af3d7a
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-09-27 12:10:01 +0300

    all combinations for invasions implemented

--- mixingcycles/dilution_InvasionGrowth.py
+++ mixingcycles/dilution_InvasionGrowth.py
@@ -9,28 +9,27 @@ import growthclasses as gc
 parser = argparse.ArgumentParser()
 parser = gc.AddGrowthParameters(parser,dilution=True)
 parser.add_argument("-m","--maxN",type=int,default=10)
-args = parser.parse_args()
+args   = parser.parse_args()
 
-g         = gc.GrowthDynamics(**vars(args))
-m         = np.arange(args.maxN)
-fp        = g.getSingleStrainFixedPointsPoissonSeeding(size = args.maxN)
-g1m1,g1m2 = g.getGrowthMatrix(size=(m,np.array([0,1])))
-g2m1,g2m2 = g.getGrowthMatrix(size=(np.array([0,1]),m))
+g                         = gc.GrowthDynamics(**vars(args))
+m                         = np.arange(args.maxN)
+fp                        = g.getSingleStrainFixedPointsPoissonSeeding(size = args.maxN)
+px,dpx                    = gc.PoissonSeedingVectors(m,fp,diff=True)
 
-px,dpx = gc.PoissonSeedingVectors(m,fp,diff=True)
+growth1_ICm1,growth2_ICm1 = g.getGrowthMatrix(size=(m,np.array([0,1])))
+growth1_IC1m,growth2_IC1m = g.getGrowthMatrix(size=(np.array([0,1]),m))
+invasiongrowth1           = g.Growth([fp[0],1])
+invasiongrowth2           = g.Growth([1,fp[1]])
 
-Egm1 = np.dot(g1m1[:,1],px[0])
-Eg1m = np.dot(g2m2[1,:],px[1])
+Egrowth1_ICm1             = np.dot(growth1_ICm1[:,1],px[0])
+Egrowth1_IC1m             = np.dot(growth1_IC1m[1,:],px[0])
+Egrowth2_ICm1             = np.dot(growth2_ICm1[:,1],px[1])
+Egrowth2_IC1m             = np.dot(growth2_IC1m[1,:],px[1])
+                          
+gamma1inv1                = invasiongrowth1[0]/fp[0]
+gamma2inv1                = invasiongrowth1[1]/fp[1]
+gamma1inv2                = invasiongrowth2[0]/fp[0]
+gamma2inv2                = invasiongrowth2[1]/fp[1]
 
-invasion1 = g.Growth([fp[0],1])
-invasion2 = g.Growth([1,fp[1]])
-
-gamma1inv1 = invasion1[0]/fp[0]
-gamma1inv2 = invasion2[0]/fp[0]
-
-gamma2inv1 = invasion1[1]/fp[1]
-gamma2inv2 = invasion2[1]/fp[1]
-
-
-print("{:f} {:f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}".format(g.growthrates[1]/g.growthrates[0],g.yieldfactors[1]/g.yieldfactors[0],Egm1,Eg1m,invasion1[0],invasion1[1],invasion2[0],invasion2[1],gamma1inv1,gamma1inv2,gamma2inv1,gamma2inv2))
+print("{:f} {:f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f} {:.6f}".format(g.growthrates[1]/g.growthrates[0],g.yieldfactors[1]/g.yieldfactors[0],fp[0],fp[1],Egrowth1_ICm1,Egrowth2_ICm1,Egrowth1_IC1m,Egrowth2_IC1m,invasiongrowth1[0],invasiongrowth1[1],invasiongrowth2[0],invasiongrowth2[1],gamma1inv1,gamma2inv1,gamma1inv2,gamma2inv2))
 
