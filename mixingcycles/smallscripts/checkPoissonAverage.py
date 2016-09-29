1581effbaac7d694f057fa53453a87002520786d
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-09-29 18:21:21 +0300

    added more approximations for poissonian averages

--- mixingcycles/smallscripts/checkPoissonAverage.py
+++ mixingcycles/smallscripts/checkPoissonAverage.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 import sys,math
 
-
+sys.path.append(sys.path[0] + "/..")
 import growthclasses as gc
 
 
@@ -21,13 +21,24 @@ a = np.arange(-args.maxA,args.maxA+args.dA,args.dA)
 n = np.arange(int(np.ceil(args.maxN/args.dN))+1)*args.dN
 m = np.arange(args.maxM)
 
+mlogm = m
+mlogm[m>0] *= np.log(m[m>0])
 
 for aval in a:
     for nval in n:
         px = gc.PoissonSeedingVectors(m,[nval])
         ma = m**aval
         ma[ma is np.nan] = 0
-        print(aval,nval,np.dot(ma,px[0]),nval**aval)
+        
+        expo = np.dot(ma,px[0])
+        eps1 = np.dot(m + (aval-1)*mlogm,px[0])
+        eps0 = np.dot(1 + (aval)*np.log(m[1:]),px[0][1:])
+        
+        apprexpo = nval**aval
+        appreps1 = nval*(1+(aval-1)*np.log(nval))
+        appreps0 = 1 + (aval)*np.log(nval)
+        
+        print(aval,nval,expo,eps1,eps0,apprexpo,appreps1,appreps0)
     print()
 
 
