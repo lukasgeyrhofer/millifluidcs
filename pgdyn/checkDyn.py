4f6dc89e89422b470c5f539e45953b4597adb816
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-11-22 12:36:03 +0200

    bugfixing timeintegrator class

--- pgdyn/checkDyn.py
+++ pgdyn/checkDyn.py
@@ -6,7 +6,7 @@ import argparse
 import sys,math
 
 
-class TimeIntegrator:
+class TimeIntegrator(object):
     # General forward integration of dynamics with Runge-Kutta method of 4th order
     # allows definition of multiple endconditions, currently implemented maximum time and one of the populations reaching zero
     def __init__(self,step = 1e-3,requiredpositive = True,initialconditions = None,dynamics = None,globaltime = 0,**kwargs):
@@ -20,9 +20,9 @@ class TimeIntegrator:
         if dynamics is None:            raise NotImplementedError
         else:                           self.dyn = dynamics
         
-        assert len(self.x) == len(self.dyn(0,self.x,self.params)), "Initial conditions and dynamics do not have identical dimensions"
+        assert len(self.x) == len(self.dyn(0,self.x,self.params)), "Dimensions of initial conditions and dynamics do not match"
             
-        self.globaltime = globaltime        
+        self.__globaltime = globaltime        
         self.__EndConditions = list()
         
     def RungeKutta4(self,xx,tt):
@@ -36,25 +36,25 @@ class TimeIntegrator:
     def IntegrationStep(self,time):
         t = 0
         while t <= time:
-            self.x = self.RungeKutta4(self.x,self.globaltime + t)
+            self.x = self.RungeKutta4(self.x,self.__globaltime + t)
             if self.__requiredpositive:
                 self.x[self.x<=0]=0
             t += self.__step
-        self.globaltime += time
+        self.__globaltime += time
     
     def IntegrateToZero(self,index):
         t = 0
         while self.x[index] > 0:
-            self.x = self.RungeKutta4(self.x,self.globaltime + t)
+            self.x = self.RungeKutta4(self.x,self.__globaltime + t)
             if self.__requiredpositive:
                 self.x[self.x<=0]=0
             t += self.__step
-        self.globaltime += t
+        self.__globaltime += t
         
-    def ResetInitialConditions(self,initialconditions):
+    def ResetInitialConditions(self,initialconditions,globaltime = 0):
         self.x = np.array(initialconditions,dtype=np.float64)
-        assert len(self.x) == len(self.dyn(0,self.x,self.params)), "Initial conditions do not match dynamics"
-        self.globaltime = 0
+        assert len(self.x) == len(self.dyn(0,self.x,self.params)), "Dimensions of initial conditions and dynamics do not match"
+        self.__globaltime = globaltime
 
     def SetEndConditionMaxTime(self,maxtime):
         if float(maxtime) >= 0:
@@ -68,11 +68,11 @@ class TimeIntegrator:
         self.__EndConditions.append(["reachzero",populationindex])
         
     def SetEndCondition(self,condition,value):
-        if condition == "maxtime":
+        if str(condition).lower() == "maxtime":
             if float(value) >= 0:
                 self.__EndConditions.append(["maxtime",float(value)])
-        elif condition == "reachzero":
-            if len(self.x) <= int(value):
+        elif str(condition).lower() == "reachzero":
+            if len(self.x) > int(value):
                 self.__EndConditions.append(["reachzero",int(value)])
         else:
             raise NotImplementedError
@@ -84,7 +84,7 @@ class TimeIntegrator:
         else:
             for ec in self.__EndConditions:
                 if ec[0] == "maxtime":
-                    if ec[1] > self.globaltime:
+                    if ec[1] < self.__globaltime:
                         terminateInteration = True
                 elif ec[0] == "reachzero":
                     if self.x[ec[1]] <= 0.:
@@ -94,12 +94,12 @@ class TimeIntegrator:
         return terminateInteration
     
     def IntegrateToEndConditions(self):
-        if self.CountEndCond > 0:
+        if self.CountEndConditions > 0:
             while not self.HasEnded():
-                self.x = self.RungeKutta4(self.x,self.globaltime)
+                self.x = self.RungeKutta4(self.x,self.__globaltime)
                 if self.__requiredpositive:
                     self.x[self.x<=0]=0
-                self.globaltime += self.__step
+                self.__globaltime += self.__step
         else:
             raise NotImplementedError
 
@@ -109,11 +109,17 @@ class TimeIntegrator:
     
     def __getattr__(self,key):
         if key == "CountEndConditions":
-            return len(self.endconditions)
+            return len(self.__EndConditions)
         elif key == "populations":
             return self.x
+        elif key == "time":
+            return self.__globaltime
         else:
             super(TimeIntegrator,self).__getattr__(self,key)
+    
+    def __getitem__(self,key):
+        if int(key) < len(self.x):
+            return self.x[int(key)]
 
 
 def notneg(a):
@@ -193,18 +199,27 @@ def main():
     params = {"eps":args.epsilon,"delta":args.delta,"kappa":args.kappa}
 
     d1 = TimeIntegrator(dynamics = DynWithPG,          step = args.integratetimestep, initialconditions = np.array([2,1e4,0]),   params = params)
-    d2 = TimeIntegrator(dynamics = DynDirect,          step = args.integratetimestep, initialconditions = np.array([2,1e4]),     params = params)
-    d3 = TimeIntegrator(dynamics = DynTwoStrainWithPG, step = args.integratetimestep, initialconditions = np.array([1,1,1e4,0]), params = params)
-    d4 = TimeIntegrator(dynamics = DynTwoStrainDirect, step = args.integratetimestep, initialconditions = np.array([1,1,1e4]),   params = params)
+    #d2 = TimeIntegrator(dynamics = DynDirect,          step = args.integratetimestep, initialconditions = np.array([2,1e4]),     params = params)
+    #d3 = TimeIntegrator(dynamics = DynTwoStrainWithPG, step = args.integratetimestep, initialconditions = np.array([1,1,1e4,0]), params = params)
+    #d4 = TimeIntegrator(dynamics = DynTwoStrainDirect, step = args.integratetimestep, initialconditions = np.array([1,1,1e4]),   params = params)
     
-    t = 0
-    while t < args.maxtime:
-        d1.IntegrationStep(args.timestep)
-        d2.IntegrationStep(args.timestep)
-        d3.IntegrationStep(args.timestep)
-        d4.IntegrationStep(args.timestep)
-        t += args.timestep
-        print("{:6.2f}".format(t),d1,d2,d3,d4)
+    
+    d1.SetEndCondition("maxtime",10)
+    d1.SetEndCondition("reachzero",1)
+    
+    d1.IntegrateToEndConditions()
+    
+    print(d1,d1[0],d1[1])
+    
+    
+    #t = 0
+    #while t < args.maxtime:
+        #d1.IntegrationStep(args.timestep)
+        #d2.IntegrationStep(args.timestep)
+        #d3.IntegrationStep(args.timestep)
+        #d4.IntegrationStep(args.timestep)
+        #t += args.timestep
+        #print("{:6.2f}".format(t),d1,d2,d3,d4)
     
     
 if __name__ == "__main__":
