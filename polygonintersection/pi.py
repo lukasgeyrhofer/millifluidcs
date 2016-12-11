739e4f081713fcfc0a08238059364437087236d6
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2016-12-11 21:59:31 +0200

    oop version of data loading

--- polygonintersection/pi.py
+++ polygonintersection/pi.py
@@ -12,79 +12,86 @@ def rescale(geom,x,y):
     return sa.affine_transform(geom,[1./x,0,0,1./y,0,0])
 
 
+class Coexistence(object):
+    def __init__(self,filenames1,filenames2,ExtendToGrowthRates = 10,CutAtYield = 10,WashoutThresholdGrowth = .6):
+        
+        if filenames1 is None:
+            raise ValueError
+        
+        assert len(filenames1) == len(filenames2)
+        
+        self.__invasioncurves = np.array([{},{}])
+        for fn in filenames1:
+            try:
+                self.__invasioncurves[0][self.extractYield(fn)] = np.genfromtxt(fn)
+            except:
+                pass
+        for fn in filenames2:
+            try:
+                self.__invasioncurves[1][self.extractYield(fn)] = np.genfromtxt(fn)
+            except:
+                pass
+        
+        self.__polygons = {}
+        self.__keys = list()
+        
+        for key in self.__invasioncurves[0]:
+            if not self.__invasioncurves[1].has_key(key):
+                raise IndexError
 
-def loadBoundaries(file1,file2):
-    try:
-        data1 = np.genfromtxt(file1)
-        data2 = np.genfromtxt(file2)
-    except:
-        return None
-    y1 = data1[:,0]
-    a1 = data1[:,1]
-    y2 = data2[:,0]
-    a2 = data2[:,1]
-    index1_1 = ((y1-1)**2).argmin()
-    index1_2 = ((y2-1)**2).argmin()
-    if y1[index1_1] < y1[index1_1+1]:   direction_curve1 =  1
-    else:                               direction_curve1 = -1
-    if y2[index1_2] < y2[index1_2+1]:   direction_curve2 =  1
-    else:                               direction_curve2 = -1
-    
-    slope1 = (y1[index1_1] - y1[index1_1 + direction_curve1])/(a1[index1_1] - a1[index1_1 + direction_curve1])
-    slope2 = (y2[index1_2] - y2[index1_2 + direction_curve2])/(a2[index1_2] - a2[index1_2 + direction_curve2])
-    
-    print slope1,slope2
-    exit(1)
-    return None
+            indexCenter1,indexCenter2 = self.getIndexCenter(key)
+            direction1 = 1
+            direction2 = 1
+            if self.__invasioncurves[0][key][indexCenter1 + 1,0] < 1:
+                direction1 = -1
+            if self.__invasioncurves[1][key][indexCenter2 + 1,0] < 1:
+                direction2 = -1
+            
+            # which is the upper curve?
+            upper = 0
+            if (self.__invasioncurves[0][key][indexCenter1 + direction1,0]-1)/(self.__invasioncurves[0][key][indexCenter1 + direction1,1] - 1) < (self.__invasioncurves[1][key][indexCenter2 + direction2,0] - 1)/(self.__invasioncurves[1][key][indexCenter2 + direction2] - 1):
+                upper = 1
+            
+            
+            # we know which is the upper curve and have all the raw data
+            # construct the polygon out of that
+            
+            y1 = self.__invasioncurves[upper][key][::-direction1,0]
+            a1 = self.__invasioncurves[upper][key][::-direction1,1]
+            y2 = self.__invasioncurves[1-upper][key][::direction2,0]
+            a2 = self.__invasioncurves[1-upper][key][::direction2,1]
+            
+            a1 = a1[y1 <= CutAtYield]
+            y1 = y1[y1 <= CutAtYield]
+            a2 = a2[y2 <= CutAtYield]
+            y2 = y2[y2 <= CutAtYield]
+            
+            a2[a2<=WashoutThresholdGrowth] = WashoutThresholdGrowth
+            
+            a = np.concatenate([a1,np.ones(2)*ExtendToGrowthRates,a2])
+            y = np.concatenate([y1,np.array([y1[-1],y2[0]]),y2])
+            
+            self.__polygons[key] = sg.Polygon(np.transpose([a,y]))
+            self.__keys.append(float(key))
+            
+    def extractYield(self,filename):
+        return filename.split("_")[1]
 
+    def getIndexCenter(self,key):
+        i1 = ((self.__invasioncurves[0][key][:,0] - 1)**2).argmin()
+        i2 = ((self.__invasioncurves[0][key][:,0] - 1)**2).argmin()
+        return i1,i2
 
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--infiles",nargs=2)
-parser.add_argument("-a","--aratio_washout",type=float,default=.1)
+parser.add_argument("-1","--infiles_strain1",nargs="*")
+parser.add_argument("-2","--infiles_strain2",nargs="*")
+parser.add_argument("-a","--ExtendToGrowthRates",type=float,default=10)
+parser.add_argument("-W","--WashoutThresholdGrowth",type=float,default=.6)
+parser.add_argument("-Y","--CutAtYield",type=float,default=10)
 args = parser.parse_args()
 
 
-try:
-    data1 = np.genfromtxt(args.infiles[0])
-    data2 = np.genfromtxt(args.infiles[1])
-except:
-    print >> sys.stderr,"could not open files"
-    exit(1)
+data = Coexistence(args.infiles_strain1,args.infiles_strain2,ExtendToGrowthRates = args.ExtendToGrowthRates, WashoutThresholdGrowth = args.WashoutThresholdGrowth, CutAtYield = args.CutAtYield)
 
-
-focalStrain = sg.Point([1,1])
-
-
-a = loadBoundaries(args.infiles[0],args.infiles[1])
-exit(1)
-
-branch1u = data1[data1[:,0] > 1]
-
-fig = pyplot.figure()
-ax = fig.add_subplot(111)
-
-
-t = 2.*np.pi*np.arange(10)/10.
-coords = np.array([np.sin(t),np.cos(t)]).transpose()
-coordsshift = coords + np.array([.5,.5])
-
-polygon1 = sg.Polygon(coords)
-polygon2 = sg.Polygon(coordsshift)
-
-
-plot_line(ax,polygon1.exterior)
-plot_line(ax,polygon2.exterior)
-#plot_line(ax,polygon1.exterior)
-
-plot_line2(ax,polygon1.intersection(polygon2).exterior)
-
-two = sg.Point([2,2])
-
-tmp = polygon1.intersection(two)
-
-print polygon1.contains(two)
-
-
-#pyplot.show()
