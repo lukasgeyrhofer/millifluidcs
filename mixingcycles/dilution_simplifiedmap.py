cbb36d80190d7d665fae3eacceaa0765cfd9c927
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-12-19 16:32:51 +0200

    cleanup code

--- mixingcycles/dilution_simplifiedmap.py
+++ mixingcycles/dilution_simplifiedmap.py
@@ -20,23 +20,20 @@ pa.add_argument("-R","--NRalpha",type=float,default=1)
 
 args = parser.parse_args()
 
+prec2   = args.precision**2
 alist   = np.exp(np.arange(np.log(args.alphamin),np.log(args.alphamax),args.alphastep))
 etalist = np.exp(np.arange(np.log(args.etamin),np.log(args.etamax),args.etastep))
-
-prec2 = args.precision**2
-
-f = np.zeros(2)
-j = np.zeros((2,2))
+f       = np.zeros(2)
+j       = np.zeros((2,2))
 
 for a,e in itertools.product(alist,etalist):
     n = np.ones(2) * (a + np.log(e))/2. # mean of n ~ O(a)
                                         # and     n ~ O(exp(eta))
                                         # as starting value
     dn = n
-    i = 0
+    i  = 0
     # NR iterations to compute fixed point numerically
     while np.sum((dn/n)**2) > prec2:        
-        #print i,n
         # first evaluation of function f(n)
         f[0] = a * (1-np.exp(-n[0])) - n[0]
         f[1] = a * np.exp(-n[0]) * (1-np.exp(-n[1])) * e - n[1]
@@ -50,17 +47,15 @@ for a,e in itertools.product(alist,etalist):
         # invert Jacobian
         iJ = np.linalg.inv(j)
         
-        
         # NR step
         dn  = -args.NRalpha * np.dot(iJ,f)
         n  += dn
         
-        #print j,iJ,f,n,dn
         # number of cells in inoculum cannot be negative
         n[n<0]=0
-        
+
+        # don't iterate infinitely
         i += 1
-        
         if i > args.maxsteps:
             break
         
