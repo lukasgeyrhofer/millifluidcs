9410d8792aacf78ec5fb32b8f75c87e98b17aaf8
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2016-12-20 15:06:37 +0200

    small changes in code

--- polygonintersection/pi.py
+++ polygonintersection/pi.py
@@ -13,7 +13,7 @@ import os.path as op
 def rescale(geom,x,y):
     return sa.affine_transform(geom,[x,0,0,y,0,0])
 
-def plotGraph(axes,polygon,col="#3465a4"):
+def PlotGraph(axes,polygon,col="#3465a4"):
     if isinstance(polygon,sg.Polygon):
         x,y = polygon.exterior.xy
         axes.plot(x,y,linewidth=2,color=col)
@@ -173,75 +173,77 @@ def RandomSamplePoints(polygon,inside = True,count = 1000):
             samplepoints.append(np.array([x,y]))
     return samplepoints
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-1","--infiles_strain1",nargs="*")
+    parser.add_argument("-2","--infiles_strain2",nargs="*")
+    parser.add_argument("-a","--ExtendToGrowthRates",type=float,default=10)
+    parser.add_argument("-W","--WashoutThresholdGrowth",type=float,default=.6)
+    parser.add_argument("-Y","--CutAtYield",type=float,default=10)
+    parser.add_argument("-s","--step",type=int,default=1)
+    parser.add_argument("-D","--baseDilutions",type=float,default=2)
+    parser.add_argument("-S","--substrate",type=float,default=1e4)
+    parser.add_argument("-v","--verbose",action="store_true",default=False)
+    parser.add_argument("-P","--StrainParameters",type=float,nargs="*",default=None)
+    parser.add_argument("-o","--outfile",default=None)
+    parser.add_argument("-G","--showGraph",default=False,action="store_true")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-1","--infiles_strain1",nargs="*")
-parser.add_argument("-2","--infiles_strain2",nargs="*")
-parser.add_argument("-a","--ExtendToGrowthRates",type=float,default=10)
-parser.add_argument("-W","--WashoutThresholdGrowth",type=float,default=.6)
-parser.add_argument("-Y","--CutAtYield",type=float,default=10)
-parser.add_argument("-s","--step",type=int,default=1)
-parser.add_argument("-D","--baseDilutions",type=float,default=2)
-parser.add_argument("-S","--substrate",type=float,default=1e4)
-parser.add_argument("-v","--verbose",action="store_true",default=False)
-parser.add_argument("-P","--StrainParameters",type=float,nargs="*",default=None)
-parser.add_argument("-o","--outfile",default=None)
-parser.add_argument("-G","--showGraph",default=False,action="store_true")
-args = parser.parse_args()
 
+    data = Coexistence(args.infiles_strain1,args.infiles_strain2,ExtendToGrowthRates = args.ExtendToGrowthRates, WashoutThresholdGrowth = args.WashoutThresholdGrowth, CutAtYield = args.CutAtYield,verbose = args.verbose)
 
-data = Coexistence(args.infiles_strain1,args.infiles_strain2,ExtendToGrowthRates = args.ExtendToGrowthRates, WashoutThresholdGrowth = args.WashoutThresholdGrowth, CutAtYield = args.CutAtYield,verbose = args.verbose)
+    coexRegion = data.getPolygon(args.baseDilutions/args.substrate)
+    baseRegion = data.getPolygon(args.baseDilutions/args.substrate)
 
-coexRegion = data.getPolygon(args.baseDilutions/args.substrate)
-baseRegion = data.getPolygon(args.baseDilutions/args.substrate)
+    centerPoint = sg.Point([1,1])
 
-centerPoint = sg.Point([1,1])
+    miA,miY,maA,maY = coexRegion.bounds
 
-miA,miY,maA,maY = coexRegion.bounds
+    StrainParameters = []
+    i = 0
+    while i < len(args.StrainParameters):
+        try:
+            StrainParameters.append(np.array([args.StrainParameters[i],args.StrainParameters[i+1]]))
+        except:
+            pass
+        i += 2
 
-StrainParameters = []
-i = 0
-while i < len(args.StrainParameters):
-    try:
-        StrainParameters.append(np.array([args.StrainParameters[i],args.StrainParameters[i+1]]))
-    except:
-        pass
-    i += 2
+    if args.showGraph:
+        fig = plt.figure(1, figsize=(5,5), dpi=90)
+        ax = fig.add_subplot(111)
+        ax.set_xscale("log")
+        ax.set_yscale("log")
 
-if args.showGraph:
-    fig = plt.figure(1, figsize=(5,5), dpi=90)
-    ax = fig.add_subplot(111)
-    ax.set_xscale("log")
-    ax.set_yscale("log")
+        PlotGraph(ax,coexRegion,col="#ff0000")
 
-    plotGraph(ax,coexRegion,col="#ff0000")
-
-for sp in StrainParameters:
-    if coexRegion.contains(sg.Point([sp[0],sp[1]])):
-        curRegion = rescale(data.getPolygon(sp[1]*args.baseDilutions/args.substrate),sp[0],sp[1])
-        if curRegion.contains(centerPoint):
-            coexRegion = coexRegion.intersection(curRegion)
-            if args.showGraph:
-                plotGraph(ax,curRegion,col="#00ff00")
+    for sp in StrainParameters:
+        if coexRegion.contains(sg.Point([sp[0],sp[1]])):
+            curRegion = rescale(data.getPolygon(sp[1]*args.baseDilutions/args.substrate),sp[0],sp[1])
+            if curRegion.contains(centerPoint):
+                coexRegion = coexRegion.intersection(curRegion)
+                if args.showGraph:
+                    PlotGraph(ax,curRegion,col="#00ff00")
+            else:
+                if args.verbose:
+                    print >> sys.stderr, "# Phasediagram of parameters ( {} : {} ) does not contain reference strain ( 1 : 1 ) in their rescaled coexistence region. Skipping ...".format(*sp)
         else:
             if args.verbose:
-                print >> sys.stderr, "# Phasediagram of parameters ( {} : {} ) does not contain reference strain ( 1 : 1 ) in their rescaled coexistence region. Skipping ...".format(*sp)
-    else:
-        if args.verbose:
-            print >> sys.stderr, "# Parameters ( {} : {} ) not contained in coexistence region of reference strain. Cannot compute multistrain phasediagram using these parameters. Skipping ...".format(*sp)
+                print >> sys.stderr, "# Parameters ( {} : {} ) not contained in coexistence region of reference strain. Cannot compute multistrain phasediagram using these parameters. Skipping ...".format(*sp)
 
-if args.showGraph:
-    plotGraph(ax,coexRegion)
-    plt.show()
+    if args.showGraph:
+        PlotGraph(ax,coexRegion)
+        plt.show()
 
-if args.outfile is None:
-    fp = sys.stderr
-else:
-    try:
-        fp = open(args.outfile,"w")
-    except:
+    if args.outfile is None:
         fp = sys.stderr
-PrintGraph(fp,coexRegion)
-fp.close()
+    else:
+        try:
+            fp = open(args.outfile,"w")
+        except:
+            fp = sys.stderr
+    PrintGraph(fp,coexRegion)
+    fp.close()
 
 
+if __name__ == "__main__":
+    main()
