c8878a839e9eb9524a577b5ba62730b1af67849c
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-01-23 16:48:59 +0200

    simplified dynamics from analytical approximation

---
+++ pgdyn/abdyn_simplified.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+import sys,math
+
+sys.path.append(sys.path[0] + '/../mixingcycles')
+import growthclasses as gc
+
+def dyn(t,x,params):
+    return np.array([
+            params['lambda']*(1-x[1])*x[0],
+            -params['eta']*x[1]*x[0]
+           ])
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b","--antibiotics",default=2,type=float)
+parser.add_argument("-n","--bacteria",default=10,type=float)
+parser.add_argument("-l","--lambda",default=1.5,type=float)
+parser.add_argument("-e","--eta",default=1e-1,type=float)
+
+parser.add_argument("-t","--integrationstep",type=float,default=1e-3)
+parser.add_argument("-o","--outputstep",type=int,default=100)
+parser.add_argument("-T","--maxtime",type=float,default=10)
+args = parser.parse_args()
+
+
+d = gc.TimeIntegrator(dynamics = dyn,initialconditions = np.array([args.bacteria,args.antibiotics]),step = args.integrationstep)
+
+d.SetEndCondition("maxtime",args.maxtime)
+
+print "%6.3f"%d.time,d
+while not d.HasEnded():
+    d.IntegrationStep(args.integrationstep * args.outputstep)
+    print d.time,d
+
+
