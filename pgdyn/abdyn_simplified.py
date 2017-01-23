50e03cef18bd46efc919426677fd821851d90874
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-01-23 16:52:28 +0200

    bugfixing

--- pgdyn/abdyn_simplified.py
+++ pgdyn/abdyn_simplified.py
@@ -18,8 +18,8 @@ def dyn(t,x,params):
 parser = argparse.ArgumentParser()
 parser.add_argument("-b","--antibiotics",default=2,type=float)
 parser.add_argument("-n","--bacteria",default=10,type=float)
-parser.add_argument("-l","--lambda",default=1.5,type=float)
-parser.add_argument("-e","--eta",default=1e-1,type=float)
+parser.add_argument("-l","--bactgrowth",default=1.5,type=float)
+parser.add_argument("-e","--abdecay",default=1e-1,type=float)
 
 parser.add_argument("-t","--integrationstep",type=float,default=1e-3)
 parser.add_argument("-o","--outputstep",type=int,default=100)
@@ -27,11 +27,12 @@ parser.add_argument("-T","--maxtime",type=float,default=10)
 args = parser.parse_args()
 
 
-d = gc.TimeIntegrator(dynamics = dyn,initialconditions = np.array([args.bacteria,args.antibiotics]),step = args.integrationstep)
-
+d = gc.TimeIntegrator(dynamics = dyn,initialconditions = np.array([args.bacteria,args.antibiotics]),step = args.integrationstep,params = {'lambda':args.bactgrowth,'eta':args.abdecay})
 d.SetEndCondition("maxtime",args.maxtime)
 
-print "%6.3f"%d.time,d
+
+
+print d.time,d
 while not d.HasEnded():
     d.IntegrationStep(args.integrationstep * args.outputstep)
     print d.time,d
