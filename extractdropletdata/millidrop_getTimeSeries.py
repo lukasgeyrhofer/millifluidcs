767bd084d0cbfb585667adb184708923e0839d09
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-04-26 15:47:23 +0300

    added forcelen parameters

--- extractdropletdata/millidrop_getTimeSeries.py
+++ extractdropletdata/millidrop_getTimeSeries.py
@@ -6,12 +6,18 @@ import sys,math
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infiles",nargs="*")
+parser.add_argument("-L","--forcelen",type=int,default=None)
 args = parser.parse_args()
 
 for filename in args.infiles:
     try:
         data = np.genfromtxt(filename,names = True, delimiter = ',', dtype = float)
         newdata = np.transpose(np.array([data['time'],data['Channel1_mean']]))
-        np.savetxt(filename.replace("csv","data"),newdata)
+        if not args.forcelen is None:
+            if len(newdata) < args.forcelen:
+                zeros = np.zeros(shape = (args.forcelen- len(newdata),2))
+                newdata = np.concatenate((newdata,zeros))
+                print "forcelength:",filename
+        np.savetxt(filename.replace("csv","data"),newdata,fmt='%.6e')
     except:
         continue
