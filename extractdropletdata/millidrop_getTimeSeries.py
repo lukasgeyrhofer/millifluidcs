93dd47c27df7db9a3cb113e02af205ae6f3fdd60
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-04-26 15:37:02 +0300

    extract density trajectory from datafiles

--- extractdropletdata/millidrop_getTimeSeries.py
+++ extractdropletdata/millidrop_getTimeSeries.py
@@ -4,28 +4,14 @@ import numpy as np
 import argparse
 import sys,math
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infiles",nargs="*")
-parser.add_argument("-T","--threshold",type=float,default=3e-2)
 args = parser.parse_args()
 
-data = dict()
-
 for filename in args.infiles:
-    print filename
     try:
-        data[filename] = np.genfromtxt(filename,delimiter=',',names = True, dtype=float)
-        newfilename = filename.replace("csv","data")
-
-        t = data[filenale]['time']
-        b = data[filename]['Channel1_mean']
-
-        
-        #print t,b
-        fp = open(newfilename,"w")
-        for dt in t:
-            print >> fp, dt
-        fp.close()
+        data = np.genfromtxt(filename,names = True, delimiter = ',', dtype = float)
+        newdata = np.transpose(np.array([data['time'],data['Channel1_mean']]))
+        np.savetxt(filename.replace("csv","data"),newdata)
     except:
         continue
