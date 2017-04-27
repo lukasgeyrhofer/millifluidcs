3e29b93f98df93333d21dbf019bacb3498815264
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-04-27 16:17:16 +0300

    allows to split timeseries in even and odd parts

--- extractdropletdata/millidrop_getTimeSeries.py
+++ extractdropletdata/millidrop_getTimeSeries.py
@@ -7,17 +7,24 @@ import sys,math
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infiles",nargs="*")
 parser.add_argument("-L","--forcelen",type=int,default=None)
+parser.add_argument("-S","--splitEvenOdd",default=False,action="store_true")
 args = parser.parse_args()
 
 for filename in args.infiles:
     try:
         data = np.genfromtxt(filename,names = True, delimiter = ',', dtype = float)
-        newdata = np.transpose(np.array([data['time'],data['Channel1_mean']]))
-        if not args.forcelen is None:
-            if len(newdata) < args.forcelen:
-                zeros = np.zeros(shape = (args.forcelen- len(newdata),2))
-                newdata = np.concatenate((newdata,zeros))
-                print "forcelength:",filename
-        np.savetxt(filename.replace("csv","data"),newdata,fmt='%.6e')
+        if args.splitEvenOdd:
+            newdata0 = np.transpose(np.array([data['time'][0::2],data['Channel1_mean'][0::2]]))
+            newdata1 = np.transpose(np.array([data['time'][1::2],data['Channel1_mean'][1::2]]))
+            np.savetxt(filename.replace(".csv","-0.data"),newdata0,fmt='%.6e')
+            np.savetxt(filename.replace(".csv","-1.data"),newdata1,fmt='%.6e')
+        else:
+            newdata = np.transpose(np.array([data['time'],data['Channel1_mean']]))
+            if not args.forcelen is None:
+                if len(newdata) < args.forcelen:
+                    zeros = np.zeros(shape = (args.forcelen- len(newdata),2))
+                    newdata = np.concatenate((newdata,zeros))
+                    print "forcelength:",filename
+            np.savetxt(filename.replace("csv","data"),newdata,fmt='%.6e')
     except:
         continue
