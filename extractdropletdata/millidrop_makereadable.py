30fb4d75df1c78ef58cca8ec0dac5dd2772bef75
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-05-02 20:04:21 +0300

    implemented restrictions in makereadable

--- extractdropletdata/millidrop_makereadable.py
+++ extractdropletdata/millidrop_makereadable.py
@@ -11,14 +11,32 @@ parser.add_argument("-i","--infiles",nargs="*")
 parser.add_argument("-t","--templatefile",default=None)
 parser.add_argument("-C","--columns",nargs="*",default=["Channel1_mean"])
 parser.add_argument("-B","--splitBackForthTrajectories",default=False,action="store_true")
+parser.add_argument("-h","--lowertimecutoff",default=None,type = float)
+parser.add_argument("-H","--uppertimecutoff",default=None,type = float)
+parser.add_argument("-s","--lowersignalcutoff",default=None,type = float)
+parser.add_argument("-S","--uppersignalcutoff",default=None,type = float)
+parser.add_argument("-c","--signalcolumn",default=0,type = float)
 args = parser.parse_args()
 
 if not "time" in args.columns:
     columns = ["time"] + args.columns
+    timecolumnoffset = 1
 else:
     columns = args.columns
+    timecolumnoffset = 0
+
+
 
 data = mdc.DropletData(infiles = args.infiles, templatefile = args.templatefile, datacolumns = columns, splitBackForthTrajectories = True)
+if not args.lowertimecutoff is None:
+    data.set_restriction("time","min",args.lowertimecutoff)
+if not args.uppertimecutoff is None:
+    data.set_restriction("time","max",args.uppertimecutoff)
+if not args.lowersignalcutoff is None:
+    data.set_restriction(args.columns[args.signalcolumn + timecolumnoffset],"min",args.lowersignalcutoff)
+if not args.uppersignalcutoff is None:
+    data.set_restriction(args.columns[args.signalcolumn + timecolumnoffset],"max",args.uppersignalcutoff)
+
 
 for label,trajectories in data:
     i = 0
