a974632dee6802c7526968195ab4a2cbb1300cbe
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-05-03 12:34:04 +0300

    extract growthrates from both exponential and logistic growth

--- extractdropletdata/millidrop_EstimateGrowthRate.py
+++ extractdropletdata/millidrop_EstimateGrowthRate.py
@@ -4,8 +4,14 @@
 import numpy as np
 import argparse
 import sys,math
+from scipy.optimize import curve_fit
 
 import millidrop_dataclass as mdc
+
+
+def logisticgrowth(t,la0,ln0,lk0,t0):
+    return np.exp(ln0 + np.exp(la0)*(t-t0)) / (1.+np.exp(ln0)*(np.exp(np.exp(la0)*(t-t0))-1.)/np.exp(lk0))
+
 
 parser = argparse.ArgumentParser()
 ioparser = argparse.add_argument_group(description = "==== I/O parameters ====")
@@ -21,12 +27,20 @@ hparser argparse.add_argument_group(description = "==== Histogram parameters ===
 hparser.add_argument("-B","--bins",default=10,type=int)
 hparser.add_argument("-R","--histogramrange",nargs=2,type=float,default=None)
 
+aparser.add_argument_group(description = "==== Algorithm parameters ====")
+aparser.add_argument("-m","--maxfev",default=5000,type=int)
+
 args = parser.parse_args()
 
 data = mdc.DropletData(infiles = args.infiles, templatefile = args.templatefile, splitBackForthTrajectories = True)
 
 if not args.restrictionfile is None:
     data.load_restrictions_from_file(args.restrictionfile)
+
+if not args.exponential and not args.logistic:
+    mode = "exponential"
+else:
+    mode = "exponential" if args.exponential else "logistic"
 
 growthrates = dict()
 
@@ -38,15 +52,24 @@ for experimentLabel, trajectories in data:
         t = trajectory[:,0] * 1e-3
         b = trajectory[:,1]
         
-        if len(t) >= 2:
-            sx  = np.sum(t)
-            sxx = np.sum(t*t)
-            sy  = np.sum(np.log(b))
-            sxy = np.sum(t * np.log(b))
-            n   = len(t)
-            gr  = (n * sxy - sx * sy)/(n*sxx - sx*sx)
+        if mode == "exponential":
+            if len(t) >= 2:
+                sx  = np.sum(t)
+                sxx = np.sum(t*t)
+                sy  = np.sum(np.log(b))
+                sxy = np.sum(t * np.log(b))
+                n   = len(t)
+                gr  = (n * sxy - sx * sy)/(n*sxx - sx*sx)
 
-            growthrates[experimentLabel].append(gr)
+                growthrates[experimentLabel].append(gr)
+                
+        elif mode == "logistic":
+                ic = np.array([np.log(abs(np.log(b[0]/b[1])/(t[0] - t[1]))),np.log(b[0]),np.log(b[-1]),t[0]])
+                fitMEAN,fitCOV = curve_fit(logisticgrowth,t,b,p0 = ic,maxfev = args.maxfev)
+                growthrates[experimentLabel].append(np.exp(fitMEAN[0]))
+
+            
+            
     
     print "{:15s} {:.4f} (± {:.4f}) 1/h".format(experimentLabel,np.mean(growthrates[experimentLabel]),np.sqrt(np.std(growthrates[experimentLabel])))
 
