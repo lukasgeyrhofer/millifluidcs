f0f9d5317a51ddf1fd60fc65f849972a9190fa20
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-05-29 12:29:33 +0300

    averaging over multiple channels

--- extractdropletdata/millidrop_AverageTrajectory.py
+++ extractdropletdata/millidrop_AverageTrajectory.py
@@ -29,7 +29,6 @@ if "time" not in args.channel:
     datacolumns = ["time"] + args.channel
 else:
     datacolumns = args.channel
-print args.channel
 
 data = mdc.DropletData(infiles = args.infiles,templatefile = args.templatefile, splitBackForthTrajectories = True, datacolumns = datacolumns)
 if not args.restrictionfile is None:
@@ -42,20 +41,17 @@ for label,trajectories in data:
             maxtime = np.max([t[-1,0] for t in trajectories if len(t) >= 1])
         else:
             maxtime = args.maxtime
-        timegrid         = np.linspace(start = args.mintime,stop = maxtime,num = args.datapoints)
-        sumtrajectories  = dict()
-        sum2trajectories = dict()
-        for column in args.channel:
-            sumtrajectories[column] = np.zeros((np.shape(trajectories[0])[1]-1,args.datapoints))
-            if args.stddev:
-                sum2trajectories = np.zeros((np.shape(trajectories[0])[1]-1,args.datapoints))
-            for t in trajectories:
-                if len(t) > 0:
-                    for i in range(1,np.shape(trajectories[0])[1]):
-                        values = np.interp(timegrid,t[:,0],t[:,i])
-                        sumtrajectories[column][i-1] += values
-                        if args.stddev:
-                            sum2trajectories[column][i-1] += values*values
+        timegrid        = np.linspace(start = args.mintime,stop = maxtime,num = args.datapoints)
+        sumtrajectories = np.zeros((np.shape(trajectories[0])[1]-1,args.datapoints))
+        if args.stddev:
+            sum2trajectories = np.zeros((np.shape(trajectories[0])[1]-1,args.datapoints))
+        for t in trajectories:
+            if len(t) > 0:
+                for i in range(1,np.shape(trajectories[0])[1]):
+                    values = np.interp(timegrid,t[:,0],t[:,i])
+                    sumtrajectories[i-1] += values
+                    if args.stddev:
+                        sum2trajectories[i-1] += values*values
             else:
                 n -= 1
     if n >= 1:
@@ -66,14 +62,12 @@ for label,trajectories in data:
         outfilename = outbasename + label + ".average"
         print "{:12s}: saving average from {:d} trajectories to file '{:s}'".format(label,n,outfilename)
         outdata = np.array([timegrid])
-        for column in args.channel:
-            outdata = np.concatenate([outdata,np.array(sumtrajectories[column]/n)],axis=0)
+        outdata = np.concatenate([outdata,np.array(sumtrajectories/n)],axis=0)
         np.savetxt(outfilename,np.transpose(outdata),fmt = '%.6e')
         if args.stddev:
             outfilename = outbasename + label + ".stddev"
             outdatastddev = np.array([timegrid])
-            for column in args.channel:
-                outdatastddev = np.concatenate([outdatastddev,np.sqrt(n*sum2trajectories[column] - sumtrajectories[column]*sumtrajectories[column])/np.sqrt(n*n-n)],axis=0)
+            outdatastddev = np.concatenate([outdatastddev,np.sqrt(n*sum2trajectories - sumtrajectories*sumtrajectories)/np.sqrt(n*n-n)],axis=0)
             np.savetxt(outfilename,np.transpose(outdatastddev),fmt = '%.6e')
             
         
