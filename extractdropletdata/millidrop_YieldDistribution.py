fa7b5f5c490d7cca0583eba690a435ddbd278fe2
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-07-03 12:03:20 +0300

    working version

--- extractdropletdata/millidrop_YieldDistribution.py
+++ extractdropletdata/millidrop_YieldDistribution.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import numpy as np
 import argparse
@@ -12,8 +13,7 @@ parser = mdc.AddCommandLineParameters(parser)
 
 parser.add_argument("-S","--lowerSignalCutoff",default=None,type=float)
 parser.add_argument("-R","--histogramRange",default = None,nargs=2,type=float)
-parser.add_argument("-b","--histogramBins",default=None,type=int)
-parser.add_argument("-v","--verbose",default = False,action="store_true")
+parser.add_argument("-b","--histogramBins",default=10,type=int)
 args = parser.parse_args()
 
 data = mdc.DropletData(**vars(args))
@@ -26,17 +26,25 @@ for experimentlabel,trajectories in data:
     finalSignal = list()
     for trajectory in trajectories:
         if not args.lowerSignalCutoff is None:
-            if trajectory[-1] > args.lowerSignalCutoff:
-                finalSignal.append(trajectory[-1])
+            if len(trajectory) > 1:
+                if trajectory[-1][1] > args.lowerSignalCutoff:
+                    finalSignal.append(trajectory[-1][1])
         else:
-            finalSignal.append(trajectory[-1])
+            if len(trajectory) > 1:
+                finalSignal.append(trajectory[-1][1])
     
-    outfilename = data.outbasename + "Yield_" + experimentlabel
-    h,b = np.histogram(finalSignal,bins = hBins,range = hRange)
-    b = b[:-1] + .5 * np.diff(b)
-    np.savetxt(np.transpose([b,h]),outfilename)
-    if args.verbose:
+    if len(finalSignal) > 1:
+        outfilename = data.outbasename + experimentlabel + ".yield"
+        h,b = np.histogram(finalSignal,bins = hBins,range = hRange)
+        b = b[:-1] + .5 * np.diff(b)
+        np.savetxt(outfilename,np.transpose([b,h]))
         
+        stat = [np.mean(finalSignal),np.std(finalSignal)]
+    else:
+        stat = [np.nan,np.nan]
+    
+    if args.verbose:
+        print "{:15s} {:.4f} (± {:.4f})".format(experimentlabel,stat[0],stat[1])
 
 
 
