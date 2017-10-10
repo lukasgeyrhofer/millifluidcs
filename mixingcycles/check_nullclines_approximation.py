329c4ed9e0118c517da08e660c3c16928e916a1c
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-10-10 18:01:21 +0300

    add numerical computation of real nullcline slope

--- mixingcycles/check_nullclines_approximation.py
+++ mixingcycles/check_nullclines_approximation.py
@@ -14,10 +14,13 @@ parser.add_argument("-d","--dilutionmin",type=float,default=2e-6)
 parser.add_argument("-D","--dilutionmax",type=float,default=5e-5)
 parser.add_argument("-K","--dilutionstep",type=float,default=2e-6)
 
+parser.add_argument("-s","--slopeoffset",type=float,default=.1)
+
 args = parser.parse_args()
 
 g  = gc.GrowthDynamics(**vars(args))
 gm = g.getGrowthMatrix(args.maxM)
+m  = np.arange(args.maxM)
 
 dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax, step = args.dilutionstep)
 
@@ -33,9 +36,24 @@ for dilution in dlist:
 
     slope1 = (1.-excessgrowth1 + fp[0])/(inv21 - fp[0])
     slope2 = (inv12-fp[1])/(1.-excessgrowth2 + fp[1])
+    
+    nx = np.arange(start = 0,stop = fp[0] + 1.,step = args.slopeoffset)
+    nx_onestep = np.zeros(len(nx))
+    for i in range(len(nx)):
+        px = gc.PoissonSeedingVectors(m,[nx[i],args.slopeoffset])
+        nx_onestep[i] = np.dot(px[1],np.dot(px[0],gm[:,:,0]))
+    
+    nullcline1 = np.interp(0,nx,nx_onestep - nx)
+    realslope1 = args.slopeoffset/(nullcline1 - fp[0])
+    
+    ny = np.arange(start = 0,stop = fp[1] + 1.,step = args.slopeoffset)
+    ny_onestep = np.zeros(len(ny))
+    for i in range(len(ny)):
+        py = gc.PoissonSeedingVectors(m,[args.slopeoffset,ny[i]])
+        ny_onestep[i] = np.dot(py[1],np.dot(py[0],gm[:,:,1]))
 
-    print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,fp[0],fp[1],inv12,inv21,excessgrowth1,excessgrowth2)
-    
-    
-    
+    nullcline2 = np.interp(0,ny,ny_onestep - ny)
+    realslope2 = args.slopeoffset/(nullcline2 - fp[1])
+
+    print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,fp[0],fp[1],inv12,inv21,excessgrowth1,excessgrowth2,realslope1,realslope2)
     
