baff74443d20dde6049553f29b73b7b6fbb24aba
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-10-10 18:27:16 +0300

    clean up code

--- mixingcycles/check_nullclines_approximation.py
+++ mixingcycles/check_nullclines_approximation.py
@@ -9,19 +9,20 @@ import growthclasses as gc
 parser = argparse.ArgumentParser()
 parser = gc.AddGrowthParameters(parser)
 
-parser.add_argument("-m","--maxM",type=int,default=50)
-parser.add_argument("-d","--dilutionmin",type=float,default=4e-6)
-parser.add_argument("-D","--dilutionmax",type=float,default=1e-4)
-parser.add_argument("-K","--dilutionstep",type=float,default=2e-6)
+parser.add_argument("-d","--dilutionmin",  type=float, default = 4e-6)
+parser.add_argument("-D","--dilutionmax",  type=float, default = 1e-4)
+parser.add_argument("-K","--dilutionstep", type=float, default = 2e-6)
 
-parser.add_argument("-s","--slopeoffset",type=float,default=.1)
+parser.add_argument("-m","--maxM",         type=int,   default = 50)
+parser.add_argument("-s","--slopeoffset",  type=float, default = .1)
+parser.add_argument("-v","--verbose",                  default = False, action = "store_true")
 
 args = parser.parse_args()
 
-g  = gc.GrowthDynamics(**vars(args))
-gm = g.getGrowthMatrix(args.maxM)
-m  = np.arange(args.maxM)
+g     = gc.GrowthDynamics(**vars(args))
+gm    = g.getGrowthMatrix(args.maxM)
 
+m     = np.arange(args.maxM)
 dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax, step = args.dilutionstep)
 
 for dilution in dlist:
@@ -29,31 +30,38 @@ for dilution in dlist:
     fp = g.getSingleStrainFixedPointsPoissonSeeding(size = args.maxM)
     px = gc.PoissonSeedingVectors(np.arange(args.maxM),fp)
 
+    # approximation
     inv21         = np.dot(gm[:,1,0],px[0,:]) * dilution
     inv12         = np.dot(gm[1,:,1],px[1,:]) * dilution
     excessgrowth1 = np.dot(gm[1:,0,0],px[0,:-1]) * dilution
     excessgrowth2 = np.dot(gm[0,1:,1],px[1,:-1]) * dilution
+    slope1        = (1.-excessgrowth1 + fp[0])/(inv21 - fp[0])
+    slope2        = (inv12-fp[1])/(1.-excessgrowth2 + fp[1])
 
-    slope1 = (1.-excessgrowth1 + fp[0])/(inv21 - fp[0])
-    slope2 = (inv12-fp[1])/(1.-excessgrowth2 + fp[1])
     
-    nx = np.arange(start = 0,stop = fp[0] + 1.,step = args.slopeoffset)
+    # compute real slopes from iteration map
+    # slope of nullcline 1 
+    nx         = np.arange(start = 0,stop = fp[0] + 1.,step = args.slopeoffset)
     nx_onestep = np.zeros(len(nx))
     for i in range(len(nx)):
-        px = gc.PoissonSeedingVectors(m,[nx[i],args.slopeoffset])
+        px            = gc.PoissonSeedingVectors(m,[nx[i],args.slopeoffset])
         nx_onestep[i] = np.dot(px[1],np.dot(px[0],gm[:,:,0]))*dilution
-        
-    nullcline1 = np.interp(0,(nx_onestep - nx)[::-1],nx[::-1])
-    realslope1 = args.slopeoffset/(nullcline1 - fp[0])
+    nullcline_intersection1 = np.interp(0,(nx_onestep - nx)[::-1],nx[::-1])
+    realslope1              = args.slopeoffset/(nullcline_intersection1 - fp[0])
+
     
-    ny = np.arange(start = 0,stop = fp[1] + 1.,step = args.slopeoffset)
+    # slope of nullcline 2
+    ny         = np.arange(start = 0,stop = fp[1] + 1.,step = args.slopeoffset)
     ny_onestep = np.zeros(len(ny))
     for i in range(len(ny)):
-        py = gc.PoissonSeedingVectors(m,[args.slopeoffset,ny[i]])
+        py            = gc.PoissonSeedingVectors(m,[args.slopeoffset,ny[i]])
         ny_onestep[i] = np.dot(py[1],np.dot(py[0],gm[:,:,1]))*dilution
+    nullcline_intersection2 = np.interp(0,(ny_onestep - ny)[::-1],ny[::-1])
+    realslope2              = (nullcline_intersection2 - fp[1])/args.slopeoffset
 
-    nullcline2 = np.interp(0,(ny_onestep - ny)[::-1],ny[::-1])
-    realslope2 = (nullcline2 - fp[1])/args.slopeoffset
 
-    print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,fp[0],fp[1],inv12,inv21,excessgrowth1,excessgrowth2,-realslope1,-realslope2,nullcline1,nullcline2)
+    if args.verbose:
+        print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,-realslope1,-realslope2,nullcline_intersection1,nullcline_intersection2,,fp[0],fp[1],inv12,inv21,excessgrowth1,excessgrowth2)
+    else:
+        print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,-realslope1,-realslope2)
     
