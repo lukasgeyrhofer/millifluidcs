2e0b6cbab6f426a3076cc8f0492b8dc6d4bf55ca
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-10-10 15:32:08 +0300

    more output

--- mixingcycles/check_nullclines_approximation.py
+++ mixingcycles/check_nullclines_approximation.py
@@ -8,25 +8,34 @@ import growthclasses as gc
 
 parser = argparse.ArgumentParser()
 parser = gc.AddGrowthParameters(parser)
-parser.add_argument("-m","--maxM",type=int,default=30)
 
+parser.add_argument("-m","--maxM",type=int,default=50)
 parser.add_argument("-d","--dilutionmin",type=float,default=2e-6)
 parser.add_argument("-D","--dilutionmax",type=float,default=5e-5)
 parser.add_argument("-K","--dilutionstep",type=float,default=2e-6)
 
 args = parser.parse_args()
 
-
 g  = gc.GrowthDynamics(**vars(args))
 gm = g.getGrowthMatrix(args.maxM)
 
-dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax + args.dilutionstep, step = args.dilutionstep)
+dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax, step = args.dilutionstep)
 
 for dilution in dlist:
     g.setDilution(dilution)
     fp = g.getSingleStrainFixedPointsPoissonSeeding(size = args.maxM)
     px = gc.PoissonSeedingVectors(np.arange(args.maxM),fp)
-    slope1 = (1./dilution - np.dot(gm[1:,0,0] - gm[:-1,0,0],px[0,:-1]))/np.dot(gm[:,1,0] - gm[:,0,0],px[0,:])
-    slope2 = np.dot(gm[1,:,1] - gm[0,:,1],px[1,:])/(1./dilution-np.dot(gm[0,1:,1] - gm[0,:-1,1],px[1,:-1]))
 
-    print '{:.6e} {:14.6e} {:14.6e}'.format(dilution,slope1,slope2)
+    inv21         = np.dot(gm[:,1,0],px[0,:]) * dilution
+    inv12         = np.dot(gm[1,:,1],px[1,:]) * dilution
+    excessgrowth1 = np.dot(gm[1:,0,0],px[0,:-1]) * dilution
+    excessgrowth2 = np.dot(gm[0,1:,1],px[1,:-1]) * dilution
+
+    slope1 = (1.-excessgrowth1 + fp[0])/(inv21 - fp[0])
+    slope2 = (inv12-fp[1])/(1.-excessgrowth2 + fp[1])
+
+    print '{:.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(dilution,-slope1,-slope2,fp[0],fp[1],inv12,inv21,excessgrowth1,excessgrowth2)
+    
+    
+    
+    
