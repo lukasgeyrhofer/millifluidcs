6c7b3ab6cd529397fd01c2a59519c080421cff29
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-11-02 15:32:42 +0200

    evaluate simpsons

--- mixingcycles/dilution_simpsons_pickle.py
+++ mixingcycles/dilution_simpsons_pickle.py
@@ -36,14 +36,6 @@ except:
 if not g.hasGrowthMatrix():
     raise ValueError,"Loaded pickle instance does not contain growthmatrix"
 
-m1,m2 = g.growthmatrixgrid
-
-m1A = np.repeat([m1],len(m2),axis=1)
-m2A = np.repeat(np.transpose([m2]),len(m1),axis=0)
-
-print m1A
-print m1A[0,2]
-exit(1)
 
 if args.dilutionmax is None:
     dlist = np.array([args.dilutionmin])
@@ -54,9 +46,26 @@ else:
         dlist = np.linspace(start = args.dilutionmin,stop = args.dilutionmax, num = args.dilutionbins)
 
 nlist = np.arange(start = 0,stop = args.maxIC,step = args.stepIC)
+m1,m2 = g.growthmatrixgrid
 
+# growth matrices
 gm1 = g.growthmatrix[:,:,0]
 gm2 = g.growthmatrix[:,:,1]
+
+# seeding matrices
+sm1 = np.array(np.repeat(np.transpose([m1]),len(m2),axis=1),dtype=float)
+sm2 = np.repeat(np.array([m2],dtype = float),len(m1),axis=0)
+
+# expressions for simpson's paradoxon
+dpp1 = np.nan_to_num(gm1/(gm1 + gm2))
+dpp2 = np.nan_to_num(gm2/(gm1 + gm2))
+
+dp1  = np.nan_to_num(sm1/(sm1 + sm2))
+dp2  = np.nan_to_num(sm2/(sm1 + sm2))
+
+dps1 = dp2 * gm1
+dps2 = dp1 * gm2
+
 
 if args.verbose:
     print g.ParameterString()
@@ -66,11 +75,23 @@ for dilution in dlist:
         print "# computing single step dynamics for D = {:e}".format(dilution)
     fp = open(args.outfile + "_D{:.3e}".format(dilution),"w")
     for x,y in itertools.product(nlist,repeat=2):
-        px = gc.PoissonSeedingVectors(mx,x)[0]
-        py = gc.PoissonSeedingVectors(my,y)[0]
-        nx = np.dot(py,np.dot(px,gm1))*dilution
-        ny = np.dot(py,np.dot(px,gm2))*dilution
-        fp.write('{} {} {} {}'.format(x,y,nx,ny))
+        p1 = gc.PoissonSeedingVectors(m1,x)[0]
+        p2 = gc.PoissonSeedingVectors(m2,y)[0]
+        
+        G1 = np.dot(p2,np.dot(p1,gm1)) * dilution
+        G2 = np.dot(p2,np.dot(p1,gm2)) * dilution
+        
+        R1 = np.dot(p2,np.dot(p1,dpp1)) * dilution
+        R2 = np.dot(p2,np.dot(p1,dpp2)) * dilution
+        
+        r1 = np.dot(p2,np.dot(p1,dp1)) * dilution
+        r2 = np.dot(p2,np.dot(p1,dp2)) * dilution
+        
+        S1 = np.dot(p2,np.dot(p1,dps1)) * dilution
+        S2 = np.dot(p2,np.dot(p1,dps2)) * dilution
+        
+        fp.write('{:6.3f} {:6.3f} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}\n'.format(x,y,G1,G2,R1,R2,r1,r2,S1,S2))
+        #                                                                                                           1 2 3  4  5  6  7  8  9  10
             
     fp.close()
             
