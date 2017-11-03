40cc61505ea32b09a78ac6aef32ba1964cc8d738
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-11-03 23:25:18 +0200

    implemeted logarithmic scaling for dilution list

--- mixingcycles/dilution_trajectories_pickle.py
+++ mixingcycles/dilution_trajectories_pickle.py
@@ -18,13 +18,14 @@ parser_io.add_argument("-v","--verbose",default=False,action="store_true")
 
 parser_dilution = parser.add_argument_group(description = "Parameters for dilution values")
 parser_dilution.add_argument("-d","--dilutionmin",type=float,default=1e-6)
-parser_dilution.add_argument("-D","--dilutionmax",type=float,default=1e-5)
-parser_dilution.add_argument("-K","--dilutionstep",type=float,default=1e-6)
+parser_dilution.add_argument("-D","--dilutionmax",type=float,default=None)
+parser_dilution.add_argument("-K","--dilutionsteps",type=int,default=10)
+parser_dilution.add_argument("-L","--dilutionlogscale",default = False, action = "store_true")
 
-parser_flowmap = parser.add_argument_group(description = "Parameters for Flowmap between mixing cycles")
+parser_flowmap = parser.add_argument_group(description = "Parameters for flowmap between mixing cycles")
 parser_flowmap.add_argument("-n","--maxIC",type=float,default=40)
 parser_flowmap.add_argument("-s","--stepIC",type=float,default=2)
-parser_flowmap.add_argument("-L","--trajectorylength",type=int,default=20)
+parser_flowmap.add_argument("-l","--trajectorylength",type=int,default=20)
 
 args = parser.parse_args()
 
@@ -38,31 +39,38 @@ if not g.hasGrowthMatrix():
 
 mx,my = g.growthmatrixgrid
 
-dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax + args.dilutionstep,step = args.dilutionstep)
+if args.dilutionmax is None:
+    dlist = np.array([args.dilutionmin])
+else:
+    if args.dilutionlogscale:
+        dlist = np.power(10,np.linspace(start = np.log10(args.dilutionmin),stop = np.log10(args.dilutionmax), num = args.dilutionsteps))
+    else:
+        dlist = np.linspace(start = args.dilutionmin,stop = args.dilutionmax,num = args.dilutionsteps)
+
 nlist = np.arange(start = 0,stop = args.maxIC,step = args.stepIC)
 
 gm1 = g.growthmatrix[:,:,0]
 gm2 = g.growthmatrix[:,:,1]
 
 if args.verbose:
-    print g.ParameterString()
+    sys.stdout.write(g.ParameterString())
 
 for dilution in dlist:
     if args.verbose:
-        print "# computing trajectories for D = {:e}".format(dilution)
+        sys.stdout.write("# computing trajectories for D = {:e}".format(dilution))
     fp = open(args.outfile + "_D{:.3e}".format(dilution),"w")
     for icx,icy in itertools.product(nlist,repeat=2):
         x = icx
         y = icy
-        print >> fp,x,y
+        fp.write("{} {}\n".format(x,y))
         for i in range(args.trajectorylength):
             px = gc.PoissonSeedingVectors(mx,x)
             py = gc.PoissonSeedingVectors(my,y)
             x = np.dot(py,np.dot(px,gm1))*dilution
             y = np.dot(py,np.dot(px,gm2))*dilution
-            print >> fp,x,y
+            fp.write("{} {}\n".format(x,y))
             if (x==0) and (y==0):
                 break
-        print >> fp
+        fp.write("\n")
     fp.close()
             
