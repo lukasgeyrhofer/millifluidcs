190c44bf01f5c6ceaad3d4f2219b9649963ea752
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-11-03 23:56:10 +0200

    copied new dilution list generation

--- mixingcycles/dilution_singlestep_pickle.py
+++ mixingcycles/dilution_singlestep_pickle.py
@@ -19,7 +19,8 @@ parser_io.add_argument("-v","--verbose",default=False,action="store_true")
 parser_dilution = parser.add_argument_group(description = "Parameters for dilution values")
 parser_dilution.add_argument("-d","--dilutionmin",type=float,default=1e-6)
 parser_dilution.add_argument("-D","--dilutionmax",type=float,default=None)
-parser_dilution.add_argument("-K","--dilutionstep",type=float,default=1e-6)
+parser_dilution.add_argument("-K","--dilutionsteps",type=int,default=10)
+parser_dilution.add_argument("-L","--dilutionlogscale",default = False, action = "store_true")
 
 parser_flowmap = parser.add_argument_group(description = "Parameters for Flowmap between mixing cycles")
 parser_flowmap.add_argument("-n","--maxIC",type=float,default=40)
@@ -39,25 +40,28 @@ mx,my = g.growthmatrixgrid
 if args.dilutionmax is None:
     dlist = np.array([args.dilutionmin])
 else:
-    dlist = np.arange(start = args.dilutionmin,stop = args.dilutionmax + args.dilutionstep,step = args.dilutionstep)
+    if args.dilutionlogscale:
+        dlist = np.power(10,np.linspace(start = np.log10(args.dilutionmin),stop = np.log10(args.dilutionmax), num = args.dilutionsteps))
+    else:
+        dlist = np.linspace(start = args.dilutionmin,stop = args.dilutionmax,num = args.dilutionsteps)
 nlist = np.arange(start = 0,stop = args.maxIC,step = args.stepIC)
 
 gm1 = g.growthmatrix[:,:,0]
 gm2 = g.growthmatrix[:,:,1]
 
 if args.verbose:
-    print g.ParameterString()
+    sys.stdout.write(g.ParameterString())
 
 for dilution in dlist:
     if args.verbose:
-        print "# computing single step dynamics for D = {:e}".format(dilution)
+        sys.stdout.write("# computing single step dynamics for D = {:e}".format(dilution))
     fp = open(args.outfile + "_D{:.3e}".format(dilution),"w")
     for x,y in itertools.product(nlist,repeat=2):
         px = gc.PoissonSeedingVectors(mx,x)[0]
         py = gc.PoissonSeedingVectors(my,y)[0]
         nx = np.dot(py,np.dot(px,gm1))*dilution
         ny = np.dot(py,np.dot(px,gm2))*dilution
-        fp.write('{} {} {} {}'.format(x,y,nx,ny))
+        fp.write('{} {} {} {}\n'.format(x,y,nx,ny))
             
     fp.close()
             
