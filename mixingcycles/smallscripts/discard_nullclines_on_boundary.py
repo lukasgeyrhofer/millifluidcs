b6522f9b18b47f7f5d5c6b11c66d7c324b809122
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-11-30 12:09:05 +0200

    more options for analyzing nullclines

--- mixingcycles/smallscripts/discard_nullclines_on_boundary.py
+++ mixingcycles/smallscripts/discard_nullclines_on_boundary.py
@@ -7,6 +7,7 @@ import sys,math
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infile")
 parser.add_argument("-o","--outfile",default=None)
+parser.add_argument("-S","--discard_single_points",default=False,action="store_true")
 args = parser.parse_args()
 
 try:
@@ -24,17 +25,24 @@ cur_traj = list()
 print_traj = True
 for line in fp.readlines():
     values = np.array(line.split(),dtype=np.float)
-    if len(values) == 0 and len(curtraj) > 0:
-        if print_traj:
-            for a,b in cur_traj:
-                out.write("{} {}\n".format(a,b))
+    if args.discard_single_points:
+        if len(values) > 0:
+            if values[0] > 0 and values[1] > 0:
+                out.write("{:.6e} {:.6e}\n".format(*values))
+        else:
             out.write("\n")
-        cur_traj = list()
-        print_traj = True
     else:
-        cur_traj.append(values)
-        if values[0] == 0 or values[1] == 0:
-            print_traj = False
+        if len(values) == 0 and len(cur_traj) > 0:
+            if print_traj:
+                for a,b in cur_traj:
+                    out.write("{:.6e} {:.6e}\n".format(a,b))
+                out.write("\n")
+            cur_traj = list()
+            print_traj = True
+        else:
+            cur_traj.append(values)
+            if values[0] == 0 or values[1] == 0:
+                print_traj = False
 
 if not args.outfile is None:
     out.close()
