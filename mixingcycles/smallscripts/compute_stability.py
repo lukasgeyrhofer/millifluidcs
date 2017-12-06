8b76b276067141a76f8cf28f9a79784d2a579651
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2017-12-06 23:50:30 +0200

    small changes

--- mixingcycles/smallscripts/compute_stability.py
+++ mixingcycles/smallscripts/compute_stability.py
@@ -8,14 +8,16 @@ def metric(x):
     return np.sqrt(np.dot(x,x))
 
 def getStability(x):
-    unstable = np.sum([1 if metric(x[:2]) >= 1 else 0, 1 if metric(x[2:4]) >= 1 else 0])
-    unstable += 3 if metric(x[2]) > 0 else 0
+    unstable = np.sum(np.where([metric(x[:2]) >= 1, metric(x[2:4]) >= 1 ],1,0))
+    if metric(x[1]) > 0:
+        unstable += 3
     return unstable
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infile",default=None)
 parser.add_argument("-o","--outfile",default=None)
+parser.add_argument("-F","--firstcolumn",default=None,type=float)
 args = parser.parse_args()
 
 if not args.infile is None:
@@ -35,14 +37,22 @@ else:
 out = np.zeros(len(data))
 
 for i,val in enumerate(data):
-    if val[3] > 0 and val[4] > 0:
+    if (val[3] > 0) and (val[4] > 0):
         ev = np.array([val[6],val[8],val[7],val[9]],dtype=np.float)
         out[i] = getStability(ev)
     else:
         out[i] = -1
 
-if not args.outfile is None:
-    np.savetxt(args.outfile,np.transpose([data[:,0],out]))
+if not args.firstcolumn is None:
+    fc = np.ones(len(out)) * args.firstcolumn
+    outdata = np.transpose([fc,data[:,0],out])
 else:
-    for d,s in np.transpose([data[:,0],out]):
-        sys.stdout.write("{:4e} {:d}\n".format(d,int(s)))
+    outdata = np.transpose([data[:,0],out])
+
+if not args.outfile is None:
+    np.savetxt(args.outfile,outdata)
+else:
+    for x in outdata:
+        for val in x:
+            sys.stdout.write(" {:6e}".format(val))
+        sys.stdout.write("\n")
