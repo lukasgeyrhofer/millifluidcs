8aefb96ec1c6b7a90027ce11afc0b519632f6e84
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2017-12-10 13:17:40 +0200

    generating starting points of different shape done in same program

--- mixingcycles/smallscripts/make_startlist_traj.py
+++ mixingcycles/smallscripts/make_startlist_traj.py
@@ -5,19 +5,37 @@ import argparse
 import sys,math
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-x","--xrange",default=[10,80],type=float,nargs=2)
-parser.add_argument("-X","--xstep",default=10,type=float)
-parser.add_argument("-y","--yrange",default=[10,60],type=float,nargs=2)
-parser.add_argument("-Y","--ystep",default=10,type=float)
+subparsers = parser.add_subparsers(dest = "shape")
+
+parser_rect = subparsers.add_parser("R")
+parser_rect.add_argument("-x","--xrange",default=[10,80],type=float,nargs=2)
+parser_rect.add_argument("-X","--xsteps",default=10,type=int)
+parser_rect.add_argument("-y","--yrange",default=[10,60],type=float,nargs=2)
+parser_rect.add_argument("-Y","--ysteps",default=10,type=int)
+
+parser_circ = subparsers.add_parser("C")
+parser_circ.add_argument("-r","--radius",default=10,type=float)
+parser_circ.add_argument("-s","--steps",default=12,type=int)
+
 args = parser.parse_args()
 
-x = np.arange(start = args.xrange[0], stop = args.xrange[1], step = args.xstep)
-y = np.arange(start = args.yrange[0], stop = args.yrange[1], step = args.ystep)
+if args.shape == "R":
+    x = np.linspace(start = args.xrange[0], stop = args.xrange[1], num = args.xsteps)
+    y = np.linspace(start = args.yrange[0], stop = args.yrange[1], num = args.ysteps)
 
-nx = len(x)
-ny = len(y)
+    nx = len(x)
+    ny = len(y)
 
-startpos = np.concatenate([np.transpose([x,np.ones(nx)*y[0]]),np.transpose([np.ones(ny-2)*x[-1],y[1:-1]]),np.transpose([x,np.ones(nx)*y[-1]]),np.transpose([np.ones(ny-2)*x[0],y[1:-1]])])
+    startpos = np.concatenate([np.transpose([x,np.ones(nx)*y[0]]),np.transpose([np.ones(ny-2)*x[-1],y[1:-1]]),np.transpose([x[::-1],np.ones(nx)*y[-1]]),np.transpose([np.ones(ny-2)*x[0],y[1:-1]])])
+elif args.shape == "C":
+    assert args.radius > 0
+    assert args.steps >= 2
+
+    angles = 2 * math.pi * np.arange(args.steps)/(1. * args.steps)
+    startpos = args.radius * np.transpose([np.sin(angles),np.cos(angles)])
+else:
+    raise NotImplementedError
+
 
 for p in startpos:
     print '{} {}'.format(*p)
