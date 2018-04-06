405ca986590b29cb0f0a3abc8196c3a0d7434d98
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2018-04-07 00:58:35 +0300

    adjust to new integrator

--- pgdyn/ab2_trajectories.py
+++ pgdyn/ab2_trajectories.py
@@ -7,8 +7,6 @@ import sys,math
 sys.path.append(sys.path[0] + '/../mixingcycles')
 import growthclasses as gc
 
-
-
 parser = argparse.ArgumentParser()
 parser = gc.AddGrowthParameters(parser,defaultmixingtime = 24)
 
@@ -22,29 +20,18 @@ parserALG = parser.add_argument_group(description = "==== Parameters for algorit
 parserALG.add_argument("-t","--integrationstep",default=1e-3,type=float)
 parserALG.add_argument("-o","--outputstep",default=100,type=int)
 
-parserIC = parser.add_argument_group(description = "==== Initial conditions ====")
-parserIC.add_argument("-N","--initialN",default=[1],type=float,nargs="*")
+parser_ic = parser.add_argument_group(description = "==== Initial conditions ====")
+parser_ic.add_argument("-N","--initialconditions",default=[1],type=float,nargs="*")
 
 args = parser.parse_args()
 
 
 g = gc.GrowthDynamicsAntibiotics2(**vars(args))
 
-ns  = g.numstrains
-lic = len(args.initialN)
-if lic < ns:    ic = np.concatenate([args.initialN,np.zeros(ns-lic)])
-else:           ic = np.array(args.initialN[:ns])
+traj = g.Trajectory(args.initialconditions,appendtime=True)
 
-t = g.Trajectory(args.integrationstep * args.outputstep,ic)
-
-for x in t:
-    print '{:.2f} {:14.6e} {:14.6e} {:14.6e} {:14.6e}'.format(*x)
-
-
-
-
-
-
+for x in traj:
+    print ' '.join(['{:14.6f}'.format(y) for y in x])
 
 
 
