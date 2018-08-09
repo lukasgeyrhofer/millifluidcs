6ddd97c202581219602a97c4ecf88a5d3d42c40d
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2018-08-09 11:48:38 +0300

    can nwo compute multiple contourvalues at once

--- mixingcycles/smallscripts/getContours.py
+++ mixingcycles/smallscripts/getContours.py
@@ -12,39 +12,44 @@ parser.add_argument("-i","--infile")
 parser.add_argument("-x","--xcolumn",default=0,type=int)
 parser.add_argument("-y","--ycolunm",default=1,type=int)
 parser.add_argument("-z","--zcolumn",default=2,type=int)
-parser.add_argument("-C","--contourvalue",default=0,type=float)
+parser.add_argument("-C","--contourvalues",default=[0],type=float,nargs="*")
+parser.add_argument("-o","--outbasename",default=None)
 args = parser.parse_args()
 
 
 try:
     data = np.genfromtxt(args.infile)
 except:
-    print >> sys.stderr,"could not open file"
-    exit(1)
+    raise IOError("could not open file '{}'".format(args.infile))
 
 x = np.unique(data[:,args.xcolumn])
 y = np.unique(data[:,args.ycolunm])
 z = data[:,args.zcolumn].reshape((len(x),len(y)))
-contours = measure.find_contours(z,args.contourvalue)
 
-for c in contours:
-    for i in range(len(c)):
-        ix = int(np.floor(c[i,0]))
-        iy = int(np.floor(c[i,1]))
-        px = c[i,0] - ix
-        py = c[i,1] - iy
+for cval in args.contourvalues:
+    if not args.outbasename is None:
+        fp = open(args.outbasename + "_{:.4e}.txt".format(cval))
+    else:
+        fp = sys.stdout
+        fp.write("# contourvalue = {:e}".format(cval))
         
-        try:
-            cx = (1.-px)*x[ix] + px*x[ix+1]
-        except:
-            cx = x[ix]
+    contours = measure.find_contours(z,cval)
+    for c in contours:
+        for i in range(len(c)):
+            ix = int(np.floor(c[i,0]))
+            iy = int(np.floor(c[i,1]))
+            px = c[i,0] - ix
+            py = c[i,1] - iy
             
-        try:
-            cy = (1.-py)*y[iy] + py*y[iy+1]
-        except:
-            cy = y[iy]
-        
-        print "{:.6f} {:.6f}".format(cx,cy)
-    print
+            try:    cx = (1.-px)*x[ix] + px*x[ix+1]
+            except: cx = x[ix]
+                
+            try:    cy = (1.-py)*y[iy] + py*y[iy+1]
+            except: cy = y[iy]
+            
+            fp.write("{:.6f} {:.6f}\n".format(cx,cy))
+        fp.write("\n")
+    if not args.outbasename is None:
+        fp.close()
 
 
